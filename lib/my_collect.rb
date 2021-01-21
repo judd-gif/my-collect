5664557a7e46c3b52f100e50285081f512d2a8ca
juddB <Juddbahoui@gmail.com>
2021-01-20 20:48:50 -0500

    commit tree 74fa570eb19b1d4f1bcbe06c4d71985826bee804

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(language)
+    new_language = []
+    i = 0
+    while i < language.length
+        new_language << yield(language[i]) 
+        i = i + 1
+    end
+    new_language
+end
